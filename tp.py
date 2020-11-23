0093ca9ca95625c7633783ee61e26a8a9c74118c
Kezgan <lautaromcasella@gmail.com>
2020-11-22 23:48:50 -0300

    Funciona pero no como debería. Dentro del archivo agregué un comentario actualizado de lo que sucede

--- tp.py
+++ tp.py
@@ -12,7 +12,7 @@ latasSobrantes = 0
 semaforo = threading.Semaphore(1)
 
 cantidadHeladeras = 3
-cantidadProveedores = 1
+cantidadProveedores = 2
 
 
 
@@ -31,14 +31,11 @@ class Heladera(threading.Thread):
 
     def hLatas(self):
         return len(self.latas)
-
-    def hayEspacio(self):
-        return (self.hBotellas() < 10) | (self.hLatas() < 15)        
+ 
 
 class Proveedores(threading.Thread):
-    def __init__(self, monitorProveedor):
+    def __init__(self):
         super().__init__()
-        self.monitorProveedor = monitorProveedor
 
     def cantidadBotellas(self):
         return random.randint(1, 10)
@@ -79,11 +76,12 @@ class Proveedores(threading.Thread):
             latasSobrantes = latasSobrantes - 1
 
     def run(self):
-        
-        with self.monitorProveedor:
-            for i in range(cantidadHeladeras):
-                
-                while (heladeras[i].hayEspacio()):
+    
+        for i in range(cantidadHeladeras):
+            
+            while (heladeras[i].hBotellas() < 10) | (heladeras[i].hLatas() < 15):
+                semaforo.acquire()
+                try:
                     self.generarCervezas()
                     self.entregarBotellas(heladeras[i])
                     self.entregarLatas(heladeras[i])
@@ -92,19 +90,31 @@ class Proveedores(threading.Thread):
                     time.sleep(2)
                     logging.info(f'Sobraron {botellasSobrantes} botellas y {latasSobrantes} latas')
                     time.sleep(2)
-                logging.info(f'La heladera {heladeras[i].id} esta llena con {heladeras[i].hBotellas()} botellas y {heladeras[i].hLatas()} latas')
-                time.sleep(2)
+                finally:
+                    semaforo.release()
+                    logging.info("Liberé semaforo")
+
+            logging.info(f'La heladera {heladeras[i].id} esta llena con {heladeras[i].hBotellas()} botellas y {heladeras[i].hLatas()} latas')
+            time.sleep(2)
+            
 
 # Intenté varias formas pero no me termina de funcionar con varios proveedores porque lo que pasa
 # es que entra el primer proveedor y hasta que no se llenan todas las heladeras no viene el siguiente.
 # Probé metiendo algun monitor/semaforo en el run de proveedores pero no hay caso.
 # Perdi bastante tiempo con esto porque encima queria manejar todo desde proveedor, pero bueno ¯\_(ツ)_/¯
 
-monitorProveedor = threading.Condition()
+#----------------------------------------------------------
+
+# Actualización:
+# Ahora agregando semaforo en el run "funciona", el tema es que si el proveedor 1 llenó la heladera 0, al pasar al proveedor 2 este se queda con la heladera 0 de antes, entonces vuelve a verificar si está llena y vuelve a tirar el mensaje de que se llenó la healdera 0. Recién cuando se vuelve al proveedor 1 pasa a la siguiente heladera. No pude arreglar esto.
+
+#listaProveedores = []
 
 for i in range(cantidadHeladeras):
     heladeras.append(Heladera(i))
 
 for i in range(cantidadProveedores):
-    Proveedores(monitorProveedor).start()
-
+    #p = Proveedores()
+    #listaProveedores.append(p)
+    #p.start()
+    Proveedores().start()
